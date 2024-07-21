abb04ec2c502d7c5a2cd4f2045690838827793ae
Bassam <bassam.badr200@gmail.com>
2024-07-21 19:46:18 +0100

    Add

--- server/modules/messages/messages.routes.js
+++ server/modules/messages/messages.routes.js
@@ -3,7 +3,7 @@ const messagesRouter = express.Router()
 
 const ctrl = require('./messages.controller')
 
-messagesRouter.post('/sms', ctrl.SendSms)
+messagesRouter.get('/sms', ctrl.SendSms)
 messagesRouter.get('/whatsapp', ctrl.SendWhatsApp)
 
 module.exports = messagesRouter
