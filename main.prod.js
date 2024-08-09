4c212b2f60d7f1cfe2fea363d589bb6b8f9cbcbb
bassamaiajeer <babdulrazzaq@iajeer.com>
2024-08-10 00:02:05 +0300

    v1.0.28

--- main.prod.js
+++ main.prod.js
@@ -1,8 +1,18 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const fs = require('fs');
 require('./server/server');
-
 const { updateElectronApp } = require('update-electron-app');
+
+// Create a log file path
+const logFilePath = path.join(app.getPath('userData'), 'error.log');
+
+// Function to log errors
+function logError(error) {
+    const errorMsg = `[${new Date().toISOString()}] ${error}\n`;
+    fs.appendFileSync(logFilePath, errorMsg, 'utf8');
+    console.error(error);  // Optionally, log to console as well
+}
 
 let mainWindow;
 
@@ -16,11 +26,14 @@ function createWindow() {
         },
     });
 
-    // Set the locale to Arabic
-    app.setLocale('ar');
-
+    // Load the start URL
     const startUrl = "http://localhost:65531";
     mainWindow.loadURL(startUrl);
+
+    // Set locale-specific settings if needed
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.webContents.executeJavaScript('document.documentElement.lang = "ar";');
+    });
 
     mainWindow.on('closed', () => {
         mainWindow = null;
@@ -31,7 +44,7 @@ app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        // app.quit();
+        app.quit();
     }
 });
 
@@ -41,4 +54,19 @@ app.on('activate', () => {
     }
 });
 
+// Catch app-level errors
+app.on('error', logError);
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (error) => {
+    logError(`Uncaught Exception: ${error.message}`);
+    logError(error.stack);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (reason, promise) => {
+    logError(`Unhandled Rejection: ${reason}`);
+});
+
+// Automatically update the app
 updateElectronApp();
--- server/database/dbconfig.js
+++ server/database/dbconfig.js
@@ -4,11 +4,13 @@ const { saveInCache, getValue } = require('../utils/cache.services')
 async function getConfig() {
     const username = await getValue({ key: 'oracle_username' })
     const password = await getValue({ key: 'oracle_password' })
-    const oracleIp = "127.0.0.1"
-    const oraclePort = "1521"//await getValue({ key: 'oracle_port' })
+    const oracleIp = "127.0.0.1" // await getValue({ key: 'doc_id' }) == "zV47v0fBCHl2MxcKxYcF" ? "http://192.168.0.114" : "127.0.0.1"
+    const oraclePort = "1521" // await getValue({ key: 'doc_id' }) == "zV47v0fBCHl2MxcKxYcF" ? await getValue({ key: 'oracle_port' }) : "1521"
     const oracleBranch = await getValue({ key: 'oracle_branch' })
 
     const connectString = oracleIp + ':' + oraclePort + '/' + oracleBranch
+
+    console.log(connectString)
 
     return {
         user: username,
